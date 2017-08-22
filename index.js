631df28611e2d0bc43cbe6ef838d7627b2b05425
Chris Nesbitt-Smith <chris@cns.me.uk>
2017-08-22 21:57:35 +0100

    improved rules engine, notify more than just chris

--- index.js
+++ index.js
@@ -1,52 +1,102 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
 const Prowl = require('node-prowl')
-
-app.use(cors())
-
-const {SUBSCRIBE, USER, PASS, MQTT, PROWL_KEY} = process.env
+const mqttWildcard = require('mqtt-wildcard');
 
 const mqtt = require('mqtt')
+const _ = require('lodash')
+
+const {USER, PASS, MQTT, CHRIS_PROWL_KEY, HANNAH_PROWL_KEY} = process.env
 const client = mqtt.connect(MQTT, {
   username: USER,
   password: PASS,
-
+  clean: true,
+  clientId: "rules_engine"
 })
 
-const prowl = new Prowl(PROWL_KEY);
+let prowl = {
+  Hannah: new Prowl(HANNAH_PROWL_KEY),
+  Chris: new Prowl(CHRIS_PROWL_KEY),
+  all: {
+    push: (message) => {
+      prowl_helper("Hannah", message)
+      prowl_helper("Chris", message)
+    }
+  }
+}
 
-client.on('connect', () => client.subscribe(SUBSCRIBE))
+client.on('connect', () => client.subscribe([
+    "owntracks/+/+/event",
+    "domoticz/out",
+    "alarm/state",
+    "alarm/new-state",
+    "presence/home/enter",
+    "presence/home/leave",
+    "/rules/presence/home/+"
+  ]
+))
 
-var response = []
+let current_alarm_state
 
 client.on('message', function (topic, message) {
-  const device = topic.split("/")[2]
-  const payload = JSON.parse(message.toString())
-  console.log(topic, payload)
+  try {
+    message = JSON.parse(message.toString())
+  }
+  catch (e) {
+    message = message.toString()
+  }
 
-  if (payload._type === "transition") {
-    if (payload.desc === "Home") {
-      prowl.push(`${device} just ${payload.event}`, 'Pinked', {
-        priority: 2,
-      }, (err, remaining) => {
-        if (err) console.error(err)
-      })
-      if (payload.event === "enter") {
-        client.publish('alarm/set-state', 'disarm', {qos: 0})
-        client.publish('domoticz/in', JSON.stringify({
-          command: "switchlight",
-          idx: 3,
-          switchcmd: "Off"
-        }), {qos: 0})
-      }
+  let t
+  if ((t = mqttWildcard(topic, 'owntracks/+/+/event')) && t !== null) {
+    let device_map = {cnsiphone: "Chris", hnsiphone: "Hannah"}
+    if (message._type === "transition" && message.desc === "Home" && device_map[t[1]]) {
+      client.publish(`presence/${message.desc.toLowerCase()}/${device_map[t[1]]}`, message.event === "enter" ? "true" : "false", {retain: true})
+      client.publish(`presence/${message.desc.toLowerCase()}/${message.event}`, device_map[t[1]])
     }
   }
 
-  response.push(message.toString())
+  // someone just got home
+  if ((t = mqttWildcard(topic, 'presence/home/enter')) && t !== null) {
+    console.log(`${message} just got home`)
+    client.publish('alarm/set-state', 'disarm')
+  }
+
+  // if people leave without setting an alarm
+  if ((t = mqttWildcard(topic, 'presence/home/leave')) && t !== null && current_alarm_state === "Disarm") {
+    console.log(`${message} left with disarmed alarm`)
+    prowl_helper(message, "You have left home but not set the alarm")
+  }
+
+  // get the retained alarm state
+  if ((t = mqttWildcard(topic, 'alarm/state')) && t !== null) {
+    console.log(`Alarm state is ${message}`)
+    current_alarm_state = message
+  }
+
+  // react to new alarm state changes
+  if ((t = mqttWildcard(topic, 'alarm/new-state')) && t !== null) {
+    console.log(`Alarm state changed to ${message}`)
+    prowl_helper("all", `Alarm state changed to ${message}`)
+    if (message === "Disarm") {
+      domoticz_helper(3, "Off")
+      domoticz_helper(51, "On")
+    }
+  }
+
+  // zwave low battery alert
+  if ((t = mqttWildcard(topic, 'domoticz/out')) && t !== null && message.Battery && message.Battery < 15) {
+    console.log(`${message.idx} is low on battery`)
+    prowl_helper("Chris", `zwave device ${message.idx} is low on battery`)
+  }
 
 })
 
-app.get('/', (req, res) => res.json(response))
+const domoticz_helper = (idx, state) => client.publish('domoticz/in', JSON.stringify({
+  command: "switchlight",
+  idx: idx,
+  switchcmd: state
+}), {qos: 0})
 
-app.listen(3002)
+const prowl_helper = (who, message) => prowl[who].push(message, 'Pinked', {
+  priority: 2,
+}, (err, remaining) => {
+  if (err) console.error(err)
+})
