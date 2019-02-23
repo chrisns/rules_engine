4841b6b90ba083fbf72e43ae94251e9aefd9039a
Chris Nesbitt-Smith <chris@cns.me.uk>
2019-02-23 00:26:34 +0000

    move chat bot message text to another file

---
+++ chatbot_messages.js
@@ -0,0 +1,85 @@
+module.exports.camera_map = {
+  camera_external_driveway: "Driveway camera",
+  camera_external_front: "Front camera",
+  camera_external_back: "Back camera",
+  camera_external_porch: "Porch camera",
+  camera_external_garden: "Garden camera"
+}
+
+module.exports.light_messages = {
+  start: "/start",
+  lounge_1_on: "Lounge On",
+  lounge_2_on: "Desk On",
+  kitchen_1_on: "Kitchen 100%",
+  kitchen_1_on_75: "Kitchen 75%",
+  kitchen_1_on_50: "Kitchen 50%",
+  kitchen_1_on_25: "Kitchen 25%",
+  kitchen_2_on: "Breakfast Bar On",
+  kitchen_3_on: "Sink On",
+  kitchen_4_on: "Dining On",
+  garden_on: "Garden On",
+  garage_1_on: "Garage On",
+  garage_2_on: "Patio On",
+  entry_light_1_on: "Hallway on",
+  entry_light_2_on: "Front house on",
+  fairy_garden_on: "Fairy garden on",
+  noah_light_on: "Noah on",
+
+  lounge_1_off: "Lounge Off",
+  lounge_2_off: "Desk Off",
+  kitchen_1_off: "Kitchen Off",
+  kitchen_2_off: "Breakfast Bar Off",
+  kitchen_3_off: "Sink Off",
+  kitchen_4_off: "Dining Off",
+  garden_off: "Garden off",
+  garage_1_off: "Garage Off",
+  garage_2_off: "Patio Off",
+  entry_light_1_off: "Hallway off",
+  entry_light_2_off: "Front house off",
+  fairy_garden_off: "Fairy garden off",
+  noah_light_off: "Noah off",
+}
+
+module.exports.velux_messages = {
+  start: "/start",
+  velux_blind_100: "Loft Blind open",
+  velux_blind_0: "Loft Blind closed",
+  velux_window_100: "Loft Window 100% Open",
+  velux_window_75: "Loft Window 75% Open",
+  velux_window_50: "Loft Window 50% Open",
+  velux_window_25: "Loft Window 25% Open",
+  velux_window_0: "Loft Window Closed",
+  velux_window_vent: "Loft Window vent open",
+}
+
+module.exports.zwave_messages = {
+  start: "/start",
+  zwave_secureadd: "Securely add device",
+  zwave_add: "Insecurely add device",
+  zwave_cancel: "Cancel controller command",
+  zwave_remove: "Remove device",
+  zwave_heal: "Heal network",
+  zwave_reset: "Soft reset controller",
+}
+
+module.exports.messages = {
+  start: "/start",
+  unlock_door: "Unlock the door",
+  unlock_garage: "Unlock the garage",
+  lock_garage: "Lock the garage",
+  disarm_alarm: "Disarm alarm",
+  arm_alarm_home: "Arm alarm home",
+  arm_alarm_away: "Arm alarm away",
+  get_alarm_status: "Get alarm status",
+  cam_driveway: "Get driveway camera",
+  cam_garden: "Get garden camera",
+  cam_porch: "Get porch camera",
+  cam_front: "Get front camera",
+  cam_back: "Get back camera",
+  all_off: "Bedtime everything off + arm home",
+  lights: "Lights",
+  velux_messages: "Velux",
+  zwave: "Z-wave management",
+  vacuum_start: "Vacuum Start",
+  vacuum_stop: "Vacuum Stop"
+}
--- index.js
+++ index.js
@@ -9,6 +9,8 @@ const Date = require("sugar-date").Date
 const memoize = require("memoizee")
 
 const { AWS_IOT_ENDPOINT_HOST, CHRIS_TELEGRAM_ID, HANNAH_TELEGRAM_ID, GROUP_TELEGRAM_ID } = process.env
+const { camera_map, light_messages, velux_messages, zwave_messages, messages } = require("./chatbot_messages")
+
 const { rulesAdd, eventHandler, pickleGherkin } = require("./rules")
 const fs = require("fs")
 const gherkin = fs.readdirSync("features").map(file => fs.readFileSync(`features/${file}`).toString()).join("\n").replace(/Feature:/ig, "#Feature:").substr(1)
@@ -205,94 +207,6 @@ const send_camera_to = (camera, who, inst_uuid = uuid(), imageBody = {}) =>
     .then(signedurl => notify_helper(who, null, null, true, signedurl))
 
 const reply_with_alarm_status = who => get_alarm_ready_status().then(ready_status => notify_helper(who, `Alarm is currently${ready_status ? " " : " not "}ready to arm`, null, true))
-
-const messages = {
-  start: "/start",
-  unlock_door: "Unlock the door",
-  unlock_garage: "Unlock the garage",
-  lock_garage: "Lock the garage",
-  disarm_alarm: "Disarm alarm",
-  arm_alarm_home: "Arm alarm home",
-  arm_alarm_away: "Arm alarm away",
-  get_alarm_status: "Get alarm status",
-  cam_driveway: "Get driveway camera",
-  cam_garden: "Get garden camera",
-  cam_porch: "Get porch camera",
-  cam_front: "Get front camera",
-  cam_back: "Get back camera",
-  all_off: "Bedtime everything off + arm home",
-  lights: "Lights",
-  velux_messages: "Velux",
-  zwave: "Z-wave management",
-  vacuum_start: "Vacuum Start",
-  vacuum_stop: "Vacuum Stop"
-}
-
-
-const zwave_messages = {
-  start: "/start",
-  zwave_secureadd: "Securely add device",
-  zwave_add: "Insecurely add device",
-  zwave_cancel: "Cancel controller command",
-  zwave_remove: "Remove device",
-  zwave_heal: "Heal network",
-  zwave_reset: "Soft reset controller",
-}
-
-const velux_messages = {
-  start: "/start",
-  velux_blind_100: "Loft Blind open",
-  velux_blind_0: "Loft Blind closed",
-  velux_window_100: "Loft Window 100% Open",
-  velux_window_75: "Loft Window 75% Open",
-  velux_window_50: "Loft Window 50% Open",
-  velux_window_25: "Loft Window 25% Open",
-  velux_window_0: "Loft Window Closed",
-  velux_window_vent: "Loft Window vent open",
-}
-
-const light_messages = {
-  start: "/start",
-  lounge_1_on: "Lounge On",
-  lounge_2_on: "Desk On",
-  kitchen_1_on: "Kitchen 100%",
-  kitchen_1_on_75: "Kitchen 75%",
-  kitchen_1_on_50: "Kitchen 50%",
-  kitchen_1_on_25: "Kitchen 25%",
-  kitchen_2_on: "Breakfast Bar On",
-  kitchen_3_on: "Sink On",
-  kitchen_4_on: "Dining On",
-  garden_on: "Garden On",
-  garage_1_on: "Garage On",
-  garage_2_on: "Patio On",
-  entry_light_1_on: "Hallway on",
-  entry_light_2_on: "Front house on",
-  fairy_garden_on: "Fairy garden on",
-  noah_light_on: "Noah on",
-
-  lounge_1_off: "Lounge Off",
-  lounge_2_off: "Desk Off",
-  kitchen_1_off: "Kitchen Off",
-  kitchen_2_off: "Breakfast Bar Off",
-  kitchen_3_off: "Sink Off",
-  kitchen_4_off: "Dining Off",
-  garden_off: "Garden off",
-  garage_1_off: "Garage Off",
-  garage_2_off: "Patio Off",
-  entry_light_1_off: "Hallway off",
-  entry_light_2_off: "Front house off",
-  fairy_garden_off: "Fairy garden off",
-  noah_light_off: "Noah off",
-
-}
-
-const camera_map = {
-  camera_external_driveway: "Driveway camera",
-  camera_external_front: "Front camera",
-  camera_external_back: "Back camera",
-  camera_external_porch: "Porch camera",
-  camera_external_garden: "Garden camera"
-}
 
 const TL_MAP = {
   chris: CHRIS_TELEGRAM_ID,
