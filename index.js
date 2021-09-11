e8df9de95e9ae02b1414478c259268a51fa8babe
Chris Nesbitt-Smith <chris@cns.me.uk>
2021-09-11 21:06:35 +0100

    remove more

--- index.js
+++ index.js
@@ -4,11 +4,7 @@ const mqttWildcard = require("mqtt-wildcard")
 const _ = require("lodash")
 const AWS = require("aws-sdk")
 const request = require("request-promise-native")
-const { v4: uuid } = require('uuid')
 const Date = require("sugar-date").Date
-const memoize = require("memoizee")
-const CronJob = require('cron').CronJob;
-
 
 const { AWS_IOT_ENDPOINT_HOST, CHRIS_TELEGRAM_ID, HANNAH_TELEGRAM_ID, GROUP_TELEGRAM_ID } = process.env
 const { camera_map, messages } = require("./chatbot_messages")
@@ -37,7 +33,6 @@ const s3 = new AWS.S3({
 const awsTopics = [
   "owntracks/+/+/event",
   "$aws/things/alarm_status/shadow/update/documents",
-  `$aws/things/${thing_lookup["Zwave eu controller"]}/shadow/update/documents`,
   `$aws/things/${thing_lookup["doorbell"]}/shadow/update/documents`,
   `$aws/things/${thing_lookup["Family bathroom lights"]}/shadow/update/documents`,
   `$aws/things/${thing_lookup["Nodeon remote"]}/shadow/update/documents`,
@@ -55,12 +50,6 @@ awsMqttClient.on("connect", () => {
   }
 })
 
-const get_alarm_state = memoize(() => iotdata.getThingShadow({ thingName: "alarm_status" }).promise()
-  .then(thing => JSON.parse(thing.payload).state.reported.state), { maxAge: 1000, length: 0, promise: true })
-
-const get_alarm_ready_status = () => iotdata.getThingShadow({ thingName: "alarm_status" }).promise()
-  .then(thing => JSON.parse(thing.payload).state.reported.ready_status)
-
 const set_alarm_state = state => iotdata.updateThingShadow({
   thingName: "alarm_status",
   payload: JSON.stringify({ state: { desired: { state: state } } })
@@ -74,17 +63,14 @@ awsMqttClient.on("message", (topic, raw_message, raw_msg, t = mqttWildcard(topic
   console.log(`Telegram user ${t[0]} just sent:"${message}"`)
 
   if (message === messages.unlock_door.toLowerCase()) zwave_helper(thing_lookup["front door lock"], { user: { Locked: false } })
-
   if (message === messages.lock_quiet.toLowerCase()) zwave_helper(thing_lookup["front door lock"], { config: { "Audio Mode": "Silent" } })
   if (message === messages.lock_noisy.toLowerCase()) zwave_helper(thing_lookup["front door lock"], { config: { "Audio Mode": "High" } })
 
   if (message === messages.arm_alarm_home.toLowerCase()) {
-    reply_with_alarm_status(t[0].toString())
     set_alarm_state("arm_home")
   }
 
   if (message === messages.arm_alarm_away.toLowerCase()) {
-    reply_with_alarm_status(t[0].toString())
     set_alarm_state("arm_away")
   }
 
@@ -97,28 +83,7 @@ awsMqttClient.on("message", (topic, raw_message, raw_msg, t = mqttWildcard(topic
 
   if (message === messages.start.toLowerCase()) notify_helper(t[0], `You can do these things`, messages)
 
-  if (message === messages.get_alarm_status.toLowerCase())
-    get_alarm_state()
-      .then(state => notify_helper(t[0], state))
-
 })
-
-const send_camera_to = (camera, who, inst_uuid = uuid(), imageBody = {}) =>
-  iotdata.getThingShadow({ thingName: camera }).promise()
-    .then(thing => JSON.parse(thing.payload).state.reported.jpg)
-
-    .then(camera_url => request({ uri: camera_url, encoding: null }))
-    .then(body => imageBody = body)
-    .then(() => s3.putObject({
-      Body: imageBody,
-      Key: `${inst_uuid}.jpg`,
-      ContentType: "image/jpeg",
-      Bucket: "me.cns.p.cams"
-    }).promise())
-    .then(() => s3.getSignedUrl("getObject", { Bucket: "me.cns.p.cams", Key: `${inst_uuid}.jpg` }))
-    .then(signedurl => notify_helper(who, null, null, true, signedurl))
-
-const reply_with_alarm_status = who => get_alarm_ready_status().then(ready_status => notify_helper(who, `Alarm is currently${ready_status ? " " : " not "}ready to arm`, null, true))
 
 const TL_MAP = {
   chris: CHRIS_TELEGRAM_ID,
@@ -193,25 +158,6 @@ const calculate_time = (number, measure) => {
 
 const clock_tic = setInterval(() => eventHandler({ topic: "clock tic" }), calculate_time(5, process.env.NODE_ENV === "production" ? "minutes" : "seconds"))
 
-var cron_schedules = []
-
-rulesAdd("cron {string}", (cron_schedule, event) => {
-  if (!cron_schedules[cron_schedule])
-    cron_schedules[cron_schedule] = new CronJob(cron_schedule, () => eventHandler({ topic: "cron", message: cron_schedule }), null, true)
-  return event.topic === "cron" && event.message === cron_schedule
-})
-
-
-rulesAdd("the {string} is reporting {string} - {string} less than {int}", async (device, genre, label, value) =>
-  await iotdata.getThingShadow({ thingName: thing_lookup[device] }).promise()
-    .then(thing => parseFloat(JSON.parse(thing.payload).state.reported[genre.toLowerCase()][label])) < value
-)
-
-rulesAdd("the {string} is reporting {word} {string} not {string}", async (device, genre, label, value, event) =>
-  event.topic === `$aws/things/${thing_lookup[device]}/shadow/update/documents` &&
-  event.message.current.state.reported[genre.toLowerCase()][label].toString() !== value.toString()
-)
-
 rulesAdd("the {string} is reporting {word} {string} {string}", async (device, genre, label, value, event) =>
   event.topic === `$aws/things/${thing_lookup[device]}/shadow/update/documents` &&
   event.message.current.state.reported[genre.toLowerCase()][label].toString() === value.toString()
@@ -234,8 +180,6 @@ rulesAdd("the {string} led strip should be {word}", async (device, action) => {
   return zwave_helper(thing_lookup[device], { "on": action })
 })
 
-
-rulesAdd("the {string} speaker should {word}", (room, action) => awsMqttClient.publish(`sonos/${action.toLowerCase()}/${room}`, JSON.stringify({}), { qos: 0 }))
 
 var backoff = {}
 rulesAdd("a {string} backoff of {int} {word}", (backoffname, time, measure) => {
